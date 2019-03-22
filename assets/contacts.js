164664d0b6e436e14409708d1309311ca1be602a
Maria Azam <M.AZ94@HOTMAIL.COM>
2019-03-21 20:25:54 -0400

    everything works except for contacts

---
+++ assets/contacts.js
@@ -0,0 +1,33 @@
+//---- API Key firebase
+
+ // Create a variable to reference the database
+ var database = firebase.database();
+
+   // Initialize Firebase
+   var config = {
+    apiKey: "AIzaSyAmE3v-l8VnrlTMnkqnw34Nr3ChSkOqof8",
+    authDomain: "bootcamp-portfolio-assignment.firebaseapp.com",
+    databaseURL: "https://bootcamp-portfolio-assignment.firebaseio.com",
+    projectId: "bootcamp-portfolio-assignment",
+    storageBucket: "bootcamp-portfolio-assignment.appspot.com",
+    messagingSenderId: "4377685241"
+  };
+  firebase.initializeApp(config);
+
+
+  //----- Add Train Form ------
+  //when submit button is clicked, the following code will occur
+  $("#submit").on("click", function(){
+
+    // inputs are stored in the variables declared above
+    var name=$("#name").val().trim();
+    var email=$("#email").val().trim();
+    var message=$("#message").val().trim();
+
+var contacts={
+    name: name,
+    email: email,
+    message: message
+}
+database.ref().push(contacts);
+});
---
+++ assets/logic.js
@@ -0,0 +1,36 @@
+//--- MAIN PAGE LOGIC
+//---- Introductory Section functions 
+
+function hi(){
+    var hi = $("<p>");
+    hi.text("Hi, ");
+    hi.addClass("myIntro");
+    $("#intro").append(hi)
+}
+function whoAmI(){
+    var whoAmI =$("<p>");
+    whoAmI.text("my name is Maria ");
+    whoAmI.addClass("myIntro")
+    $("#intro").append(whoAmI);
+}
+
+function whatDoIDo(){
+    var whatDoIDo= $("<p>");
+    whatDoIDo.text("and I'm a Web Developer. ");
+    whatDoIDo.addClass("myIntro");
+    $("#intro").append(whatDoIDo);
+}
+function viewMyWork(){
+    var viewMyWork = $("<a>");
+    viewMyWork.text("View My Work");
+    viewMyWork.addClass("myWork");
+    viewMyWork.attr("href", "#portfolio");
+    $("#intro").append(viewMyWork);
+}
+
+//---- Introductory Div functioning -----
+setTimeout(hi, 1000);
+setTimeout(whoAmI, 2500);
+setTimeout(whatDoIDo, 4000);
+setTimeout(viewMyWork,5500);
+    
